d452eded1e1723b35a28d9a6894331bd2c8303bd
Matheus Rian <souzamatheusrian@gmail.com>
2023-07-15 12:35:31 -0300

    feat: add Type Domain in of DatabaseRepository

--- ride/src/infra/repository/DriverRepositoryDatabase.ts
+++ ride/src/infra/repository/DriverRepositoryDatabase.ts
@@ -1,5 +1,7 @@
 import { Pool } from "pg";
 import { DriverRepository } from "../../application/repository/DriverRepository";
+import Driver from "../../domain/Driver";
+import { UUID } from "../../application/usecase/models/uuid";
 
 const pool = new Pool({
   host: '0.0.0.0',
@@ -12,15 +14,15 @@ const pool = new Pool({
 export default class DriverRepositoryDatabase implements DriverRepository {
   constructor () {}
 
-  async save(driver: any) {
+  async save(driver: Driver) {
     const client = await pool.connect();
 		const query = 'INSERT INTO cccat12.driver (driver_id, name, email, document, car_plate) VALUES ($1, $2, $3, $4, $5)';
-		const values = [driver.driverId, driver.name, driver.email, driver.document, driver.carPlate];
+		const values = [driver.driverId, driver.name, driver.email.value, driver.document.value, driver.carPlate.value];
     await client.query(query, values);
     client.release();	
   }
 
-  async get(driverId: string) {
+  async get(driverId: UUID): Promise<Driver> {
     const client = await pool.connect();
     const { rows: driverData } = await client.query(
       "SELECT * FROM cccat12.driver WHERE driver_id = $1",
@@ -28,6 +30,12 @@ export default class DriverRepositoryDatabase implements DriverRepository {
     );
     
     client.release();
-    return driverData;
+    return new Driver(
+      driverData[0].driver_id,
+      driverData[0].name,
+      driverData[0].email,
+      driverData[0].document,
+      driverData[0].car_plate,
+    );
   } 
 } 
--- ride/src/infra/repository/PassengerRepositoryDatabase.ts
+++ ride/src/infra/repository/PassengerRepositoryDatabase.ts
@@ -1,5 +1,7 @@
 import { Pool } from "pg";
 import { PassengerRepository } from "../../application/repository/PassengerRepository";
+import { UUID } from "../../application/usecase/models/uuid";
+import Passenger from "../../domain/Passenger";
 
 const pool = new Pool({
   host: '0.0.0.0',
@@ -12,16 +14,16 @@ const pool = new Pool({
 export default class PassengerRepositoryDatabase implements PassengerRepository {
   constructor () {}
 
-  async save(passenger: any) {
+  async save(passenger: Passenger) {
     const client = await pool.connect();
     const query = 'INSERT INTO cccat12.passenger (passenger_id, name, email, document) VALUES ($1, $2, $3, $4)';
-		const values = [passenger.passengerId, passenger.name, passenger.email, passenger.document];
+		const values = [passenger.passengerId, passenger.name, passenger.email.value, passenger.document.value];
 	
 		await client.query(query, values);
 		client.release();	
   }
 
-  async get(passengerId: string) {
+  async get(passengerId: UUID): Promise<Passenger> {
     const client = await pool.connect();
     const { rows: passengerData } = await client.query(
       "SELECT * FROM cccat12.passenger WHERE passenger_id = $1",
@@ -29,6 +31,11 @@ export default class PassengerRepositoryDatabase implements PassengerRepository 
     );
     
     client.release();
-    return passengerData;
+    return new Passenger(
+      passengerData[0].passenger_id,
+      passengerData[0].name,
+      passengerData[0].email,
+      passengerData[0].document,
+    );
   } 
 } 
