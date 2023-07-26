53595cf62dcb79907897e0a74ed0b5803c18d560
Matheus Rian <souzamatheusrian@gmail.com>
2023-07-26 18:10:23 -0300

    test: add coordinates position in Ride

---
+++ ride/test/unit/DistanceCalculator.spec.ts
@@ -0,0 +1,9 @@
+import Coord from "../../src/domain/Coord";
+import DistanceCalculator from "../../src/domain/DistanceCalculator";
+
+test('Should calculate the distance beetween two coordinates', () => {
+  const from = new Coord(-8.087602480277932, -34.94039886110192);
+  const to = new Coord(-7.998906652322922, -34.97156026714327);
+  const distance = DistanceCalculator.calculate(from, to);
+  expect(distance).toBe(10);
+})
--- ride/test/unit/Ride.spec.ts
+++ ride/test/unit/Ride.spec.ts
@@ -3,47 +3,42 @@ import Ride from "../../src/domain/Ride";
 
 test("Deve fazer o cálculo do preço de uma corrida durante o dia", function () {
 	const ride = new Ride();
-	ride.addSegment(10, new Date("2021-03-01T10:00:00"));
+	ride.addPosition(-8.087602480277932, -34.94039886110192, new Date("2021-03-01T10:00:00"));
+	ride.addPosition(-7.998906652322922, -34.97156026714327, new Date("2021-03-01T10:00:00"));
 	expect(ride.calculate()).toBe(21);
 });
 
 test("Deve fazer o cálculo do preço de uma corrida durante a noite", function () {
 	const ride = new Ride();
-	ride.addSegment(10, new Date("2021-03-01T23:00:00"));
+	ride.addPosition(-8.087602480277932, -34.94039886110192, new Date("2021-03-01T23:00:00"));
+	ride.addPosition(-7.998906652322922, -34.97156026714327, new Date("2021-03-01T23:00:00"));
 	expect(ride.calculate()).toBe(39);
 });
 
 test("Deve fazer o cálculo do preço de uma corrida no domingo de dia", function () {
 	const ride = new Ride();
-	ride.addSegment(10, new Date("2021-03-07T10:00:00"));
+	ride.addPosition(-8.087602480277932, -34.94039886110192, new Date("2021-03-07T10:00:00"));
+	ride.addPosition(-7.998906652322922, -34.97156026714327, new Date("2021-03-07T10:00:00"));
 	expect(ride.calculate()).toBe(29);
 });
 
 test("Deve fazer o cálculo do preço de uma corrida no domingo de noite", function () {
 	const ride = new Ride();
-	ride.addSegment(10, new Date("2021-03-07T23:00:00"));
+	ride.addPosition(-8.087602480277932, -34.94039886110192, new Date("2021-03-07T23:00:00"));
+	ride.addPosition(-7.998906652322922, -34.97156026714327, new Date("2021-03-07T23:00:00"));	
 	expect(ride.calculate()).toBe(50);
 });
 
-test("Deve retornar -1 se a distância for inválida", function () {
+test("Deve lançar um erro se a data for inválida", function () {
 	const ride = new Ride();
-	expect(() => ride.addSegment(-10, new Date("2023-03-01T10:00:00"))).toThrow(new Error("Invalid distance"));
-});
-
-test("Deve retornar -2 se a data for inválida", function () {
-	const ride = new Ride();
-	expect(() => ride.addSegment(10, new Date("javascript"))).toThrow(new Error("Invalid date"));
+	ride.addPosition(-8.087602480277932, -34.94039886110192, new Date("JavaScript"));
+	ride.addPosition(-7.998906652322922, -34.97156026714327, new Date("JavaScript"));
+	expect(() => ride.calculate()).toThrow(new Error("Invalid date"));
 });
 
 test("Deve fazer o cálculo do preço de uma corrida durante o dia com preço mínimo", function () {
 	const ride = new Ride();
-	ride.addSegment(3, new Date("2021-03-01T10:00:00"));
+	ride.addPosition(-8.087602480277932, -34.94039886110192, new Date("2021-03-07T10:00:00"));
+	ride.addPosition(-8.078526196150117, -34.93453928495122, new Date("2021-03-07T10:00:00"));	
 	expect(ride.calculate()).toBe(10);
-});
-
-test("Deve fazer o cálculo do preço de uma corrida com múltiplos segmentos", function () {
-	const ride = new Ride();
-	ride.addSegment(10, new Date("2021-03-01T10:00:00"));
-	ride.addSegment(10, new Date("2021-03-01T10:00:00"));
-	expect(ride.calculate()).toBe(42);
 });
