aad2a845c2a2f6ee937917250bc52de0ec4f93c9
Matheus Rian <souzamatheusrian@gmail.com>
2023-07-27 13:38:31 -0300

    feat: create method update in controller rideRepositoryDatabase

--- ride/src/infra/repository/RideRepositoryDatabase.ts
+++ ride/src/infra/repository/RideRepositoryDatabase.ts
@@ -5,6 +5,7 @@ import DatabaseConnection from "../database/DatabaseConnection";
 
 export default class RideRepositoryDatabase implements RideRepository {
   constructor (readonly connection: DatabaseConnection) {}
+
   async save(ride: Ride): Promise<void> {
     await this.connection.query(
       "insert into cccat12.ride (ride_id, passenger_id, from_lat, from_long, to_lat, to_long, status, request_date) values ($1, $2, $3, $4, $5, $6, $7, $8)",
@@ -13,13 +14,21 @@ export default class RideRepositoryDatabase implements RideRepository {
   }
   async get(rideId: string): Promise<Ride> {
     const { rows: rideData } = await this.connection.query('select * from cccat12.ride where ride_id = $1', [rideId]);
-    return new Ride(
+    const ride = new Ride(
       rideData[0].ride_id,
       rideData[0].passenger_id,
       new Coord(parseFloat(rideData[0].from_lat), parseFloat(rideData[0].from_long)),
       new Coord(parseFloat(rideData[0].to_lat), parseFloat(rideData[0].to_long)),
       rideData[0].status,
       rideData[0].request_date,
-    )
+    );
+    ride.driverId = rideData[0].driver_id;
+    ride.acceptDate = rideData[0].accept_date;
+    return ride;
+  }
+
+  async update(ride: Ride): Promise<void> {
+    await this.connection.query('update cccat12.ride set driver_id = $1, status = $2, accept_date = $3 where ride_id = $4',
+    [ride.driverId, ride.status, ride.acceptDate, ride.rideId]);
   }
 }
