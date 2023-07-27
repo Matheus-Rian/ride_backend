7d27cc05bcffc88c7686e51703c7bb88f62b8735
Matheus Rian <souzamatheusrian@gmail.com>
2023-07-26 21:12:57 -0300

    feat: using pattern Chain of responsability in segments

--- ride/src/domain/ride/Ride.ts
+++ ride/src/domain/ride/Ride.ts
@@ -1,4 +1,9 @@
 import DistanceCalculator from '../distance/DistanceCalculator';
+import FareCalculatorHandler from '../fare/chain_of_responsability/FareCalculatorHandler';
+import NormalFareCalculatorHandler from '../fare/chain_of_responsability/NormalFareCalculatorHandler';
+import OvernightFareCalculatorHandler from '../fare/chain_of_responsability/OvernightFareCalculatorHandler';
+import OvernightSundayFareCalculatorHandler from '../fare/chain_of_responsability/OvernightSundayFareCalculatorHandler';
+import SundayFareCalculatorHandler from '../fare/chain_of_responsability/SundayFareCalculatorHandler';
 import FareCalculatorFactory from '../fare/strategy/FareCalculatorFactory';
 import Position from './Position';
 import Segment from './Segment';
@@ -6,9 +11,14 @@ import Segment from './Segment';
 export default class Ride {
 	positions: Position[];
 	MIN_PRICE = 10;
+	fareCalculator: FareCalculatorHandler;
 
 	constructor () {
 		this.positions = [];
+		const overnightSundayFireCalculator = new OvernightSundayFareCalculatorHandler();
+		const sundayFireCalculator = new SundayFareCalculatorHandler(overnightSundayFireCalculator);
+		const overnightFireCalculator = new OvernightFareCalculatorHandler(sundayFireCalculator);
+		this.fareCalculator = new NormalFareCalculatorHandler(overnightFireCalculator);
 	}
 
 	addPosition(lat: number, long: number, date: Date) {
@@ -22,8 +32,9 @@ export default class Ride {
 			if (!nextPosition) break;
 			const distance = DistanceCalculator.calculate(position.coord, nextPosition.coord);
 			const segment = new Segment(distance, nextPosition.date);
-			const fareCalculator = FareCalculatorFactory.create(segment);
-			price += fareCalculator.calculate(segment);
+			price += this.fareCalculator.handle(segment);
+			// const fareCalculator = FareCalculatorFactory.create(segment);
+			// price += fareCalculator.calculate(segment);
 		}
 		return (price < this.MIN_PRICE) ? this.MIN_PRICE : price;
 	}
