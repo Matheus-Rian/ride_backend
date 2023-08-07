53a2ceeb65c5dbb714284c1e8e9cdf750fc08944
Matheus Rian <souzamatheusrian@gmail.com>
2023-08-07 12:14:21 -0300

    feat: using registry in controller and api

--- ride/src/infra/http/MainController.ts
+++ ride/src/infra/http/MainController.ts
@@ -1,51 +1,46 @@
 // O que seria um controller? Recebe eventos e repassa para a aplicação 
 
+import UsecaseFactory from '../../application/factory/UsecaseFactory';
 import { CalculateRide } from '../../application/usecase/CalculateRide';
-import { CreateDriver } from '../../application/usecase/CreateDriver';
 import { CreatePassenger } from '../../application/usecase/CreatePassenger';
-import { GetDriver } from '../../application/usecase/GetDriver';
-import { GetPassenger } from '../../application/usecase/GetPassenger';
-import { RequestRide } from '../../application/usecase/RequestRide';
+import Registry from '../di/Registry';
+import inject from '../di/inject';
 import HttpServer from './HttpServer';
 
 // Interface Adapter
 export default class MainController {
-  constructor (
-    httpServer: HttpServer,
-    calculateRide: CalculateRide,
-    createPassenger: CreatePassenger,
-    getPassenger: GetPassenger,
-    createDriver: CreateDriver,
-    getDriver: GetDriver,
-    requestRide: RequestRide    
-  ) {
+  @inject('calculateRide')
+  calculateRide?: CalculateRide
+  @inject('createPassenger')
+  createPassenger?: CreatePassenger;
+  constructor (httpServer: HttpServer, usecaseFactory: UsecaseFactory) {
     httpServer.on('post', '/calculate_ride', async (params: any, body: any) => {
-      const output = await calculateRide.execute(body);
+      const output = await this.calculateRide?.execute(body);
       return output;
     });
     
     httpServer.on('post', '/passengers', async (params: any, body: any) => {
-      const output = await createPassenger.execute(body);
+      const output = await this.createPassenger?.execute(body);
       return output;
     });
     
     httpServer.on('get', '/passengers/:passengerId', async (params: any, body: any) => {
-      const output = await getPassenger.execute({ passengerId: params.passengerId });
+      const output = await usecaseFactory.createGetPassenger().execute({ passengerId: params.passengerId });
       return output;
     });
     
     httpServer.on('post', '/drivers', async (params: any, body: any) => {
-      const output = await createDriver.execute(body);
+      const output = await usecaseFactory.createCreateDriver().execute(body);
       return output;
     });
     
     httpServer.on('get', '/drivers/:driverId', async (params: any, body: any) => {
-      const output = await getDriver.execute({ driverId: params.driverId });
+      const output = await usecaseFactory.createGetDriver().execute({ driverId: params.driverId });
       return output;
     });
 
     httpServer.on('post', '/request_ride', async (params: any, body: any) => {
-      const output = await requestRide.execute(body);
+      const output = await usecaseFactory.createRequestRide().execute(body);
       return output;
     });
   }
--- ride/src/main_api.ts
+++ ride/src/main_api.ts
@@ -11,28 +11,31 @@ import MainController from "./infra/http/MainController";
 import ExpressAdapter from "./infra/http/ExpressAdapter";
 import { RequestRide } from "./application/usecase/RequestRide";
 import RideRepositoryDatabase from "./infra/repository/RideRepositoryDatabase";
+import UsecaseFactory from "./application/factory/UsecaseFactory";
+import RepositoryFactoryDatabase from "./infra/factory/RepositoryFactoryDatabase";
+import Registry from "./infra/di/Registry";
 
 // Main composition root
 const connection = new PgaAdapter();
 const passangerRepository = new PassengerRepositoryDatabase(connection);
-const driverRepository = new DriverRepositoryDatabase(connection);
-const rideRepository = new RideRepositoryDatabase(connection);
+// const driverRepository = new DriverRepositoryDatabase(connection);
+// const rideRepository = new RideRepositoryDatabase(connection);
 
 const calculateRide = new CalculateRide();
 const createPassenger = new CreatePassenger(passangerRepository);
-const getPassenger = new GetPassenger(passangerRepository);
-const createDriver = new CreateDriver(driverRepository);
-const getDriver = new GetDriver(driverRepository);
-const requestRide = new RequestRide(rideRepository)
+// const getPassenger = new GetPassenger(passangerRepository);
+// const createDriver = new CreateDriver(driverRepository);
+// const getDriver = new GetDriver(driverRepository);
+// const requestRide = new RequestRide(rideRepository)
 const httpServer = new ExpressAdapter();
+const repositoryFactory = new RepositoryFactoryDatabase(connection);
+const usecaseFactory = new UsecaseFactory(repositoryFactory);
+const registry = Registry.getInstance();
+registry.provide('calculateRide', calculateRide);
+registry.provide('createPassenger', createPassenger);
 
 new MainController(
 	httpServer,
-	calculateRide,
-	createPassenger,
-	getPassenger,
-	createDriver,
-	getDriver,
-	requestRide
+	usecaseFactory,
 );
 httpServer.listen(3000);
